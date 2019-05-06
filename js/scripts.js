75b343d4fd08dd977c497c012c42e68cf50f1c59
insoul937 <31006119+insoul937@users.noreply.github.com>
2019-05-06 11:05:46 +0500

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+var $ = jQuery;
+$(document).ready(function(){
+    $(".small-block ul li").click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        var index = $(this).index();
+        $('.large-block ul li').eq(index).addClass('active').siblings().removeClass('active');
+    });
+    $('.btn-sb').click(function () {
+        $(this).parent().find('.invisible-filter').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+    $('.sel').select2();
+    $('.top-carousel').owlCarousel({
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        items: 1,
+        smartSpeed: 800,
+        margin: 0,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        loop: true,
+    });
+    $('.catalog-carousel').owlCarousel({
+        items: 4,
+        smartSpeed: 800,
+        margin: 33,
+        nav: true,
+        navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+        dots: false,
+        autoplay: false,
+        loop: true,
+    });
+    if($(".checkbox").length > 0 || $(".radio").length > 0){
+        checks();
+        $(".checkbox input[type='checkbox'], .radio input[type='radio']").change(function(){
+            checks();
+        });
+    }
+    $('.symbol-list a').click(function(){
+        $('.symbol-list a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+function checks(){
+    $(".checkbox input[type='checkbox'], .radio input[type='radio']").each(function(){
+        if($(this).is(':checked')){
+            $(this).closest('label').addClass('checked');
+        }
+        else {
+            $(this).closest('label').removeClass('checked');
+        }
+    });
+}
+
+
+
+
+
